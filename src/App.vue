c31ebfd264e479a8c70f981486c5bd9663374401
main1118 <375277023@qq.com>
2020-12-14 16:20:52 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App asfasf"/>
   </div>
 </template>
 
@@ -12,7 +12,7 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }ACCESS_TOKEN
+  }
 }
 </script>
 
